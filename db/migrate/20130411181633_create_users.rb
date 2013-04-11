e7a5ba87443d8a6f582db3e56a6c2117d42e04bb
Apprentice <apprentice@devbootcamp.com>
2013-04-11 12:07:07 -0700

    tests written for user model

--- db/migrate/20130411181633_create_users.rb
+++ db/migrate/20130411181633_create_users.rb
@@ -1,6 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.integer :time_donated, :default => 0
+      t.string :password_digest
+      t.boolean :admin, :default => false
 
       t.timestamps
     end
