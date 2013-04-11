6d845f4ce5d95635947b22f19588b123a3c867ee
Kevin Ko + Philip Woo <jhw1202+kk+pw@gmail.com>
2013-04-11 15:00:48 -0700

    create and show for auction. auction end date is hard coded for now

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -5,9 +5,17 @@ class AuctionsController < ActionController::Base
   end
 
   def create
+    @auction = Auction.new(params[:auction])
+    @auction.end_date = DateTime.now + 14 # TODO Hardcoded end date for now
+    if @auction.save
+      redirect_to auction_path @auction
+    else
+      render :new
+    end
   end
 
   def show
+    @auction = Auction.find(params[:id])
   end
 
   def edit
