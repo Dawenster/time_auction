7d84e1a65dcfa5b28267116122a1e84ec0d650b2
Apprentice <apprentice@devbootcamp.com>
2013-04-11 11:15:02 -0700

    second first commit...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeAuction::Application.config.secret_token = 'df555dde7eb5235a098d14befcb9e55ee4ee97e0f4a311bafea594fa026e67932a544308b5837b7b22808d4f85bae2e87434f49f95e97ef12b377ba2a02f7e31'
