eb639dc42dd80ebd8a28c7ed789ff6eacdc5fda2
Kevin Ko + Philip Woo <jhw1202+kk+pw@gmail.com>
2013-04-11 14:18:24 -0700

    added model migration test for category

---
+++ app/models/category.rb
@@ -0,0 +1,4 @@
+class Category < ActiveRecord::Base
+  attr_accessible :name
+  validates :name, :presence => true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130411182821) do
+ActiveRecord::Schema.define(:version => 20130411211637) do
 
   create_table "auctions", :force => true do |t|
     t.string   "title"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(:version => 20130411182821) do
     t.datetime "deadline"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
