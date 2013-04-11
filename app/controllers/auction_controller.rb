c7fcf926c90e92bf4003529552ebb165ea662619
Kevin Ko + Philip Woo <jhw1202+kk+pw@gmail.com>
2013-04-11 14:13:21 -0700

    added routes/controller/actions/new view for auction

---
+++ app/controllers/auction_controller.rb
@@ -0,0 +1,20 @@
+class AuctionController < ActionController::Base
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+  
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TimeAuction::Application.routes.draw do
+  resources :auctions
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
