c5235aba6110341ec7dd49aef0b231d694398e19
Kevin Ko + Philip Woo <jhw1202+kk+pw@gmail.com>
2013-04-11 11:48:58 -0700

    working on creating deadline

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -5,5 +5,10 @@ class Auction < ActiveRecord::Base
 
   validates_presence_of :title, :description, :end_date, :category_id, :deadline
 
+  before_save :create_deadline
+
+  def create_deadline
+    self.deadline = self.end_date.month + 1 
+  end
   
 end
