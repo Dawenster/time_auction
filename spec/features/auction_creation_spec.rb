34bba46bd6aa3e497df35f409ee99a3cfd00bc7a
Kevin Ko + Philip Woo <jhw1202+kk+pw@gmail.com>
2013-04-11 17:16:44 -0700

    integration test for new auction form

--- spec/features/auction_creation_spec.rb
+++ spec/features/auction_creation_spec.rb
@@ -5,23 +5,28 @@ describe "AuctionCreation" do
   subject { page }
 
   describe "creating auction" do
-    before { visit new_user_path } do
+    before do
+      visit new_user_path 
       fill_in :user_name, with: "Test Name"
       fill_in :user_email, with: "j@example.com"
       fill_in :user_phone, with: "310-333-3333"
       fill_in :user_password, with: "Password1"
+      click_button "Create User"
     end
 
     it "creates auction with valid input" do
       visit new_auction_path
-      fill_in :title, with: "Test Title"
-      fill_in :description, with: "Super valid description"
-      select('Experiences', :from => 'Category') # gon fail
-      fill_in :month, with: '04'
+      fill_in "Title", with: "Test Title"
+      fill_in "Description", with: "Super valid description"
+      page.select('Experiences', :from => "auction_category_id")
+      click_button 'Create Auction'
+      page.should have_content "Test Title"
+    end
 
-
+    it "doesn't create an auction with invalid input" do
+      visit new_auction_path
+      click_button 'Create Auction'
+      page.should have_content "Add an Auction"
+    end
   end
-end
-
-
 end
