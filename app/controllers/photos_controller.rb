72cb207d206aefae2f9c8a90ad32883e591def66
Kevin Ko + Philip Woo <jhw1202+kk+pw@gmail.com>
2013-04-12 16:56:20 -0700

    wip: constructed photocontroller

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,26 @@
+class PhotosController < ApplicationController
+  def create
+    if params[:user_id]
+      @imageable = User.find(params[:user_id])
+    elsif params[:auction_id]
+      @imageable = Auction.find(params[:user_id])
+    else # TODO: Placeholder for inevitable Charity imageable
+      return false
+    end
+
+    @photo = @imageable.photos.build(:url => params[:file]
+
+    p = Photo.new
+    p.url = params[:file]
+
+    if @imageable.class = "Auction" 
+      p.imageable_type = "Auction" 
+      p.save!
+      redirect_to auction_path(@imageable)
+    else
+      p.imageable_type = "User"
+      p.save!
+      redirect_to user_path(@imageable)
+    end
+  end
+end
