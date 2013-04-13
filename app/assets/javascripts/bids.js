e26c91b3fa84dcb2f61a9393280be44e84826484
Apprentice <apprentice@devbootcamp.com>
2013-04-13 16:28:02 -0700

    Finish avatars in nav bar

--- app/assets/javascripts/bids.js
+++ app/assets/javascripts/bids.js
@@ -14,17 +14,17 @@ $(document).ready(function() {
   });
 
   $('#trigger').click(function(){
-      $('#new_bid_form').fadeIn(200);
-      $('#overlay').fadeIn(200);
+      $('#new_bid_form').fadeIn(50);
+      $('#overlay').fadeIn(50);
   });
   
   $('#overlay').click(function(){
-      $('#new_bid_form').fadeOut(200);
-      $('#overlay').fadeOut(200);
+      $('#new_bid_form').fadeOut(50);
+      $('#overlay').fadeOut(50);
   });
 
   $('.commit-to-bid').click(function(){
-      $('#new_bid_form').fadeOut(200);
-      $('#overlay').fadeOut(200);
+      $('#new_bid_form').fadeOut(50);
+      $('#overlay').fadeOut(50);
   });
 });
