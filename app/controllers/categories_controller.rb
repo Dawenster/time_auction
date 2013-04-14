28a30dfba3f865783444d74dd374e59d4fd15211
Kevin Ko + Philip Woo <jhw1202+kk+pw@gmail.com>
2013-04-13 19:48:16 -0700

    wip categories page has tabs

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,10 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    @auctions_categories = Hash.new(Array.new)
+    @categories.each do |category|
+      @auctions_categories[category.name] = category.auctions
+    end
   end
 
   def show
