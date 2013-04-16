340f1699f697d8e8f7eba690f71c76b6fc2e9127
Apprentice <apprentice@devbootcamp.com>
2013-04-16 16:00:27 -0700

    Bid History complete restyle

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,7 +8,7 @@ module UsersHelper
     end
   end
 
-  def avatar(user, options = {})
+  def navatar(user, options = {})
     height = options.fetch(:height, 25)
     width = options.fetch(:width, 25)
     link_id = options.fetch(:link_id, "navName")
@@ -31,6 +31,21 @@ module UsersHelper
         :gravity => :face, :crop => :thumb) + current_user.name.split(" ").first, 
         user_path(current_user), :id => link_id
       link + admin_note
+    end
+  end
+
+  def avatar(user, options = {})
+    height = options.fetch(:height, 48)
+    width = options.fetch(:width, 48)
+    
+    if user.photo_url
+      link_to (cl_image_tag current_user.photo_url,#.gsub('large', 'square'), 
+        :width => width, :height => height, 
+        :gravity => :face, :crop => :thumb)
+    else
+      link_to (cl_image_tag 'v1365894993/avatar_blank_q1pbbs.jpg', 
+        :width => width, :height => height, 
+        :gravity => :face, :crop => :thumb)
     end
   end
 end
