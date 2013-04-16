01d3f2a3583df0e7be3e19ba639044e5e913e43f
Apprentice <apprentice@devbootcamp.com>
2013-04-15 18:49:39 -0700

    user images now partials in nav bar, some reformatting on user show

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,23 @@ module UsersHelper
 
   def profile_pic(user, width = 200, height = 200)
     if user.photo_url
-      cl_image_tag user.photo_url, :width => width, :height => height, :crop => :fill
+      cl_image_tag user.photo_url, :width => width, :height => height, :crop => :pad
     else
       image_tag '/assets/avatar_blank.jpg'
+    end
+  end
+
+  def avatar(user, width = 25, height = 25)
+    if user.photo_url
+      link_to (cl_image_tag current_user.photo_url.gsub('large', 'square'), 
+      :width => width, :height => height, 
+      :gravity => :face, :crop => :thumb) + current_user.name.split(" ").first, 
+      user_path(current_user), :id => "navName"
+    else
+      link_to (cl_image_tag 'v1365894993/avatar_blank_q1pbbs.jpg', 
+      :width => width, :height => height, 
+      :gravity => :face, :crop => :thumb) + current_user.name.split(" ").first, 
+      user_path(current_user), :id => "navName"
     end
   end
 end
