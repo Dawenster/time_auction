a9765383ffe4c730f49e799ad590feb1abdb48dc
J Connolly <jsconnolly1@gmail.com>
2013-04-17 16:04:42 -0700

    login form saves email on invalid login attempt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,7 @@ class SessionsController < ApplicationController
       redirect_back_or @user
     else
       flash.now[:error] = "Invalid email or password"
+      @session_email = params[:session][:email]
       render "new"
     end
   end
