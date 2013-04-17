50fbd7c26c5701b7d93cc70f7ddcb7c2ad0c2663
Apprentice <apprentice@devbootcamp.com>
2013-04-16 20:11:56 -0700

    more incremental stuff

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,6 @@ class CategoriesController < ApplicationController
 
   def index
     @auctions_categories = Auction.includes(:category).all.group_by {|a| a.category.name}
-    @categories = Category.all
   end
 end
 
