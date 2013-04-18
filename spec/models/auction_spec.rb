3b3c9e0ca9ee678a6db80fc64d4e5d1738d58bdc
jhw1202 <jhw1202@gmail.com>
2013-04-17 21:45:49 -0700

    more auction model testing

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Auction do
 
   let!(:auction) {FactoryGirl.create(:auction) }
+  let!(:one_hour_bid) {FactoryGirl.create(:bid)}
   subject { auction }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
@@ -14,5 +15,15 @@ describe Auction do
 
   it "should create a deadline based on the end date" do
     auction.deadline.should_not be_blank
+  end
+
+  it "should initialize with zero verified hours" do
+    auction.verified_time.should == 0
+  end
+
+  it "should return highest bid when highest_bid method is called" do
+    one_hour_bid.update_attribute(:auction_id, auction.id)
+    higher_bid = Bid.create(time:5, auction_id: auction.id, user_id: 2)
+    auction.highest_bid.should == higher_bid
   end
 end
