d270a4ba8a42ae2f1cddfbf67f5132d2cd95e359
jhw1202 <jhw1202@gmail.com>
2013-04-17 23:07:37 -0700

    cant run test on category index; errors out because no photo on factory auction create

--- spec/features/category_index_spec.rb
+++ spec/features/category_index_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
-describe "Category index page" do
+describe "Category index page", :js => true do
 	let!(:auction) {FactoryGirl.create(:auction)}
+	
 	before do
-		visit categories_path
+		
 	end
 
-	it "should display "
+	# it "should display auctions for corresponding category" do
+	# 	category = Category.find_by_name("People")
+	# 	auction.update_attribute(:category_id, category.id)
+	# 	visit categories_path
+	# 	find(:xpath, "//a[@href='#people']").click
+	# 	page.should have_content auction.title
+	# end
 end
