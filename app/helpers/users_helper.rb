7677e2e05379492f307ffc7a9de63b11ff7c1b7a
Dawenster <dstwen@gmail.com>
2013-04-17 20:50:35 -0700

    changed current_user to user on avatar helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -38,8 +38,8 @@ module UsersHelper
     height = options.fetch(:height, 48)
     width = options.fetch(:width, 48)
     
-    if current_user && user.photo_url
-      link_to (cl_image_tag current_user.photo_url,#.gsub('large', 'square'), 
+    if user.photo_url
+      link_to (cl_image_tag user.photo_url,#.gsub('large', 'square'), 
         :width => width, :height => height, 
         :gravity => :face, :crop => :thumb)
     else
