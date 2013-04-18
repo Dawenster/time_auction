274eb45b8646c97b80c1c79bb36b9aafba18deaa
J Connolly <jsconnolly1@gmail.com>
2013-04-17 18:03:32 -0700

    WIP pwd reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,10 @@
 class PasswordResetsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:email])
+    user.send_password_reset if user
+    redirect_to root_url, notice: 'Email with password reset instructions sent!'
+  end
 end
